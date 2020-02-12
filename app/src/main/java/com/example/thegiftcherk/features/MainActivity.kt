1223d6ea12f77f8b7dc425068bafac99583000bc
Alessandro Pace <alexpace6@gmail.com>
2020-02-12 20:50:27 +0100

    Prueba
    Comentario de prueba en main

--- app/src/main/java/com/example/thegiftcherk/features/MainActivity.kt
+++ app/src/main/java/com/example/thegiftcherk/features/MainActivity.kt
@@ -30,6 +30,8 @@ class MainActivity : AppCompatActivity() {
             )
         )
 
+        //Comentario de Prueba
+
 
         setSupportActionBar(materialToolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
