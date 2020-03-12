7c3b41f70906fbc6ab58d0e68383f1cf1f5b43e6
Alessandro Pace <alexpace6@gmail.com>
2020-03-12 19:37:08 +0100

    colors added

---
+++ app/src/main/java/com/example/thegiftcherk/features/ui/home/HomeAdapter.kt
@@ -0,0 +1,30 @@
+package com.example.thegiftcherk.features.ui.home
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thegiftcherk.R
+import com.example.thegiftcherk.features.ui.search.models.Item
+import com.example.thegiftcherk.setup.utils.extensions.inflate
+
+
+class HomeAdapter(
+    private val items: MutableList<Item>,
+    private val listener: (Item) -> Unit
+) : RecyclerView.Adapter<HomeHolder>() {
+    override fun onBindViewHolder(
+        filterHolder: HomeHolder,
+        position: Int
+    ) {
+        items[position].apply { filterHolder.bind(this) { listener(it) } }
+    }
+
+    //region Methods
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeHolder {
+        val inflatedView = parent.inflate(R.layout.item_friend, false)
+
+        return HomeHolder(inflatedView)
+    }
+
+    override fun getItemCount(): Int = items.size
+    //endregion
+}
---
+++ app/src/main/java/com/example/thegiftcherk/features/ui/home/HomeHolder.kt
@@ -0,0 +1,47 @@
+package com.example.thegiftcherk.features.ui.home
+
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
+import com.example.thegiftcherk.features.ui.search.models.Item
+import com.example.thegiftcherk.setup.utils.extensions.logD
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.item_row.view.*
+import kotlinx.coroutines.flow.callbackFlow
+
+class HomeHolder(v: View) : RecyclerView.ViewHolder(v) {
+
+    private var view: View = v
+    private var item: Item? = null
+    private lateinit var listener: (Item) -> Unit
+
+    init {
+        v.setOnClickListener {
+            item?.let {
+                listener(it)
+                logD("Click from holder")
+            }
+        }
+    }
+
+    fun bind(item: Item, listener: (Item) -> Unit) {
+        this.item = item
+        this.listener = listener
+
+        view.subject.text = item.id
+        view.date.text = item.name
+        view.message.text = item.description
+
+        Picasso.get()
+            .load(item.picture)
+            .noPlaceholder()
+            .into(view.itemImage)
+
+
+
+
+    }
+
+
+}
