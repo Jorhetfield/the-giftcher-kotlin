f29f5f270a7ccaee16e49f10a6df94fac9682337
Alessandro Pace <alexpace6@gmail.com>
2020-03-28 12:46:25 +0100

    date picker simplified

--- app/src/main/java/com/example/thegiftcherk/features/ui/login/RegisterFragment.kt
+++ app/src/main/java/com/example/thegiftcherk/features/ui/login/RegisterFragment.kt
@@ -1,12 +1,12 @@
 package com.example.thegiftcherk.features.ui.login
 
 import android.app.DatePickerDialog
-import android.app.Dialog
 import android.app.TimePickerDialog
-import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
+import android.content.DialogInterface
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.example.thegiftcherk.R
 import com.example.thegiftcherk.features.ui.main.MainActivity
 import com.example.thegiftcherk.setup.BaseFragment
@@ -16,16 +16,10 @@ import com.example.thegiftcherk.setup.utils.extensions.addTenths
 import com.example.thegiftcherk.setup.utils.extensions.isEmail
 import com.example.thegiftcherk.setup.utils.extensions.isValidPassword
 import com.example.thegiftcherk.setup.utils.extensions.logD
-import com.google.android.material.textfield.TextInputLayout
 import kotlinx.android.synthetic.main.dialog_date_picker.*
-import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_register.*
 import kotlinx.android.synthetic.main.fragment_register.buttonConfirm
 import kotlinx.android.synthetic.main.fragment_register.constraintContainer
-import kotlinx.android.synthetic.main.fragment_register.inputEmail
-import kotlinx.android.synthetic.main.fragment_register.inputEmailLayout
-import kotlinx.android.synthetic.main.fragment_register.inputPassword
-import kotlinx.android.synthetic.main.fragment_register.inputPasswordLayout
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -87,22 +81,25 @@ class RegisterFragment : BaseFragment() {
 
     private fun checkInputs(): Boolean {
         return if (inputEmail?.text.toString().isEmail() && inputEmail?.text.toString().isNotEmpty()
-            && inputPassword?.text.toString().isNotEmpty() && inputPassword?.text.toString().length >= 6
+            && inputPassword?.text.toString()
+                .isNotEmpty() && inputPassword?.text.toString().length >= 6
         ) {
             true
         } else {
             if (inputEmail?.text.toString().isEmpty() && inputPassword?.text.toString().isEmpty()) {
                 showError(getString(R.string.error_pass), constraintContainer)
-            }else if (inputPassword?.text.toString().isValidPassword() ) {
+            } else if (inputPassword?.text.toString().isValidPassword()) {
                 showError(getString(R.string.error_pass), constraintContainer)
-            }else if (!inputEmail?.text.toString().isEmail() || inputEmail?.text.toString().isEmpty()) {
+            } else if (!inputEmail?.text.toString().isEmail() || inputEmail?.text.toString()
+                    .isEmpty()
+            ) {
                 showError(getString(R.string.error_email), constraintContainer)
             }
             false
         }
     }
 
-    private fun showDate(date: String){
+    private fun showDate(date: String) {
         birthdayText.text = date
     }
 
@@ -113,76 +110,29 @@ class RegisterFragment : BaseFragment() {
         val today = calendar.get(Calendar.DAY_OF_MONTH)
         val nowHour = calendar.get(Calendar.HOUR_OF_DAY)
         val nowMinute = calendar.get(Calendar.MINUTE)
+        var stringDate = ""
 
-        context?.let { context ->
-            Dialog(context).apply {
-                requestWindowFeature(Window.FEATURE_NO_TITLE)
-                setCancelable(true)
-                setContentView(R.layout.dialog_date_picker)
-                //Dialog
-                buttonClose?.setOnClickListener {
-                    cancel()
-                }
-                buttonConfirm?.setOnClickListener { view ->
-                    if (textDate?.text.toString().isNotBlank() &&
-                        textHourSelected?.text.toString().isNotBlank()
-                    ) {
-                        logD("logprueba ${textDate.text}")
-                        prefs.fcmToken = textDate.text.toString()
-                        logD("logpruebaprefs ${prefs.fcmToken}")
-                        showDate(textDate.text.toString())
-                        cancel()
-                    } else {
-                        showError(getString(R.string.error_pass), view)
-                    }
-                }
-                buttonDate?.setOnClickListener {
-                    val dpd = DatePickerDialog(
-                        context,
-                        R.style.DialogTheme,
-                        DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-                            textDate?.text = String.format(
-                                "%s/%s/%s",
-                                year.toString().addTenths(),
-                                (month + 1).toString().addTenths(),
-                                dayOfMonth.toString().addTenths()
-                            )
-                        },
-                        2000,
-                        12,
-                        11
-                    )
-                    dpd.datePicker
-                    dpd.datePicker.minDate = System.currentTimeMillis() - 1000
-                    dpd.show()
-                }
-                buttonHour?.setOnClickListener {
-                    val tpd = TimePickerDialog(
-                        context,
-                        TimePickerDialog.OnTimeSetListener { _, hour, minute ->
-                            textHourSelected.text = String.format(
-                                "%s:%s",
-                                hour.toString().addTenths(),
-                                minute.toString().addTenths()
-                            )
-                        },
-                        nowHour,
-                        nowMinute,
-                        true
-                    )
-                    tpd.show()
-                }
-                //Change window options:
-                val lp = WindowManager.LayoutParams()
-                lp.copyFrom(window?.attributes)
-                lp.width = WindowManager.LayoutParams.MATCH_PARENT
-                lp.height = WindowManager.LayoutParams.MATCH_PARENT
-                window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-                show()
-                window?.attributes = lp
-            }
-        }
+        val dpd = DatePickerDialog(
+            context!!,
+            R.style.DialogTheme,
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                stringDate = String.format(
+                    "%s/%s/%s",
+                    year.toString().addTenths(),
+                    (month + 1).toString().addTenths(),
+                    dayOfMonth.toString().addTenths()
+                )
+                showDate(stringDate)
+            },
+            thisYear,
+            month,
+            today
+        )
+        dpd.datePicker.minDate = -System.currentTimeMillis()
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 1000
+        dpd.show()
     }
+
 
     private fun requestRegister(email: String, pass: String, name: String, phone: String) {
         GlobalScope.launch(Dispatchers.Main) {
